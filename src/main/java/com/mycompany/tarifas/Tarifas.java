7cdfa5db5461c9f3549ae0786b17e857b39f7b4b
JordyQbitw <jordy.quimbita2005@gmail.com>
2024-02-16 14:37:58 -0500

    first

---
+++ src/main/java/com/mycompany/tarifas/Tarifas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarifas;
+
+/**
+ *
+ * @author labctr
+ */
+public class Tarifas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
